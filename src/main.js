c504b3269e2fc85d1e5076f8403bf5b523050722
Oluwaseunus <oluwaseunus@gmail.com>
2019-07-16 00:57:17 +0100

    Starting work on components

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import BootStrapVue from 'bootstrap-vue';
 
-Vue.config.productionTip = false
+import App from './App.vue';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(BootStrapVue);
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount('#app');
